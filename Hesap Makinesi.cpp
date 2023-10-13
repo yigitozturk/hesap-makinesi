810f54ec6bc16a5b1bef56e4a5bd8466e766cccd
Yiğit Taha Öztürk <77507219+yigitozturk@users.noreply.github.com>
2023-10-13 15:57:42 +0300

    Add files via upload

---
+++ Hesap Makinesi.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	float sayi1, sayi2;
+	string islem;
+
+	cout << "Lutfen Isleminizi Seciniz" << endl << endl;
+	cout << "Toplama (+)" << endl;
+	cout << "Cikarma (-)" << endl;
+	cout << "Bolme (/)" << endl;
+	cout << "Carpma (*)" << endl;
+	cout << "Us Alma (^)" << endl;
+	cout << "Karekok Alma (V)" << endl << endl;
+	cin >> islem;
+	cout << endl;
+
+	if (islem == "+")
+	{
+		cout << "Lutfen Birinci Sayiyi Giriniz = ";
+		cin >> sayi1;
+		cout << endl;
+		cout << "Lutfen Ikinci Sayiyi Giriniz = ";
+		cin >> sayi2;
+		cout << endl;
+		cout << "Isleminizin Sonucu = " << sayi1 + sayi2 << endl;
+	}
+	else if (islem == "-")
+	{
+		cout << "Lutfen Birinci Sayiyi Giriniz = ";
+		cin >> sayi1;
+		cout << endl;
+		cout << "Lutfen Ikinci Sayiyi Giriniz = ";
+		cin >> sayi2;
+		cout << endl;
+		cout << "Isleminizin Sonucu = " << sayi1 - sayi2 << endl;
+	}
+	else if (islem == "/")
+	{
+		cout << "Lutfen Birinci Sayiyi Giriniz = ";
+		cin >> sayi1;
+		cout << endl;
+		cout << "Lutfen Ikinci Sayiyi Giriniz = ";
+		cin >> sayi2;
+		cout << endl;
+		cout << "Isleminizin Sonucu = " << sayi1 / sayi2 << endl;
+	}
+	else if (islem == "*")
+	{
+		cout << "Lutfen Birinci Sayiyi Giriniz = ";
+		cin >> sayi1;
+		cout << endl;
+		cout << "Lutfen Ikinci Sayiyi Giriniz = ";
+		cin >> sayi2;
+		cout << endl;
+		cout << "Isleminizin Sonucu = " << sayi1 * sayi2 << endl;
+	}
+	else if (islem == "^")
+	{
+		cout << "Lutfen Birinci Sayiyi Giriniz = ";
+		cin >> sayi1;
+		cout << endl;
+		cout << "Lutfen Ikinci Sayiyi Giriniz = ";
+		cin >> sayi2;
+		cout << endl;
+		cout << "Isleminizin Sonucu = " << pow(sayi1, sayi2) << endl;
+	}
+	else if (islem == "V")
+	{
+		cout << "Lutfen Karekokunu Almak Istediginiz Sayiyi Giriniz = ";
+		cin >> sayi1;
+		cout << endl;
+		cout << "Isleminizin Sonucu = " << sqrt(sayi1) << endl;
+	}
+	else
+	{
+		cout << "Hatali Bir Sembol Girdiniz" << endl;
+	}
+	return 0;
+
+}
